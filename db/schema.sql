bbbbdbbcab8b7cd1993084198d4fa64b816ab9e1
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-10-07 15:59:21 -0400

    reuploaded the database

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,21 @@ DROP TABLE IF EXISTS members;
 
 CREATE TABLE members (
   user_id SERIAL unique PRIMARY KEY ,
-  name VARCHAR(50) NOT NULL,
+  fname VARCHAR(50) NOT NULL,
+  lname VARCHAR(50) NOT NULL,
   email TEXT NOT NULL,
   password_digest TEXT NOT NULL DEFAULT 'temp123',
   address TEXT NOT NULL,
   city TEXT NOT NULL,
   state TEXT NOT NULL,
   zipcode INT NOT NULL,
-  member_id INT NOT NULL DEFAULT now(),
+  member_id INT NOT NULL DEFAULT 00000,
   active BOOLEAN NOT NULL DEFAULT false,
   user_created TIMESTAMP NOT NULL DEFAULT now()
 );
 
 
-CREATE INDEX ON members(name);
+CREATE INDEX ON members(fname);
 CREATE INDEX ON members(email);
 
 CREATE TABLE saved_events (
