39a52fe109109007e1e229963525df3e2cc15734
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-10-07 14:02:11 -0400

    members uploaded

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS saved_events;
 DROP TABLE IF EXISTS members;
 
 CREATE TABLE members (
   user_id SERIAL unique PRIMARY KEY ,
   name VARCHAR(50) NOT NULL,
   email TEXT NOT NULL,
-  password_digest TEXT NOT NULL,
+  password_digest TEXT NOT NULL DEFAULT 'temp123',
   address TEXT NOT NULL,
   city TEXT NOT NULL,
   state TEXT NOT NULL,
@@ -14,12 +15,13 @@ CREATE TABLE members (
   user_created TIMESTAMP NOT NULL DEFAULT now()
 );
 
+
 CREATE INDEX ON members(name);
 CREATE INDEX ON members(email);
 
 CREATE TABLE saved_events (
   saved_id SERIAL PRIMARY KEY,
-  user_reference INTEGER REFERENCES, members(user_id),
+  user_reference INTEGER REFERENCES members(user_id),
   title TEXT NOT NULL,
   image_link TEXT,
   event_date DATE NOT NULL,
